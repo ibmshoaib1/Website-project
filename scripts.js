462f6dfbc5fe5db83e9bf10abf4ae07f912aab5b
ibmshoaib1 <shoaibknsit11@gmail.com>
2025-02-04 10:14:40 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,26 @@
-// Example JavaScript for additional functionality
 $(document).ready(function(){
     // Initialize carousel
     $('.carousel').carousel({
         interval: 5000
     });
 
-    // WhatsApp popup after 5 seconds
+    // Show popup after 5 seconds
     setTimeout(function(){
-        alert("Please contact us on WhatsApp for more information!");
+        $('#userDataModal').modal('show');
     }, 5000);
+
+    // Handle form submission
+    $('#userDataForm').on('submit', function(e){
+        e.preventDefault();
+        const userName = $('#userName').val();
+        const userEmail = $('#userEmail').val();
+        const userPhone = $('#userPhone').val();
+
+        // Send data to server or store locally
+        console.log({ userName, userEmail, userPhone });
+
+        // Close modal
+        $('#userDataModal').modal('hide');
+        alert('Thank you for submitting your details!');
+    });
 });
